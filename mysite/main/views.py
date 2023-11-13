ed2d41e8196a92631f7949ff21a69f3414ae1ef2
Marc Engle <marcengle30@gmail.com>
2023-11-13 16:28:27 -0500

    added templates/explored admin page

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -5,5 +5,7 @@ from .models import ToDoList, Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
-    return HttpResponse("<h2>%s</h2><br><p>%s</p>" %(ls.name, str(item.text)))
+    return render(response, "main/list.html", {"ls": ls})
+
+def home(response):
+    return render(response, "main/home.html", {})
